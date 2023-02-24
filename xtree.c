67591831b08c43e69024a0bf35dd97b1f6970f42
R3Z0NE <rafox71321@gmail.com>
2023-02-24 21:59:23 +0100

    No handling of CLI arguments

--- xtree.c
+++ xtree.c
@@ -5,45 +5,26 @@
 
 int main(int argc, char *argv[])
 {
+    long width = 10;      // Default width: 10
+    char character = 'x'; // default character: "x"
 
-    long width = 10; // Default width: 10
-    char base = argv[1][0]; // TODO: Default character
-    errno = 0;
-    char *p;
-
-    if (argc == 3)
+    // Execute until number of characters to write is greater or equal to provided width
+    for (int i = width, chars_amount = 1; chars_amount <= width; chars_amount += 2)
     {
-        width = strtol(argv[2], &p, 10);
-    }
-
-    if (errno != 0 || *p != '\0' || width > 100 || width < 1)
-    {
-        printf("Error processing your 2nd CLI argument (width of the tree) (int).\n");
-        printf("Enter value between 1-100.\n");
-        return 1;
-    }
-
-    else
-    {
-
-        // Execute until number of characters to write is greater or equal to provided width
-        for (int i = width, chars_amount = 1; chars_amount <= width; chars_amount += 2)
+        for (int k = 0; k < ((i - chars_amount) / 2); k++) // Empty characters before
         {
-            for (int k = 0; k < ((i - chars_amount) / 2); k++) // Empty characters before
-            {
-                printf(" ");
-            }
-
-            for (int l = 0; l < chars_amount; l++) // Print out provided character 'chars_amount' times.
-            {
-                printf("%c", base);
-            }
-
-            for (int k = 0; k < ((i - chars_amount) / 2); k++) // Empty characters after
-            {
-                printf(" ");
-            }
-            printf("\n"); // End of the line
+            printf(" ");
         }
+
+        for (int l = 0; l < chars_amount; l++) // Print out provided character 'chars_amount' times.
+        {
+            printf("%c", character);
+        }
+
+        for (int k = 0; k < ((i - chars_amount) / 2); k++) // Empty characters after
+        {
+            printf(" ");
+        }
+        printf("\n"); // End of the line
     }
 }
