3cf1e62429423607115a949e1e9036b27f4b1ee6
R3Z0NE <rafox71321@gmail.com>
2023-02-24 22:25:34 +0100

    Added CLI arguments handling

--- xtree.c
+++ xtree.c
@@ -5,10 +5,31 @@
 
 int main(int argc, char *argv[])
 {
+    // If none CLI argument are privided, these variables remain untouched
     long width = 10;      // Default width: 10
     char character = 'x'; // default character: "x"
 
+    if (argc >= 3) // If 'character' and 'width' CLI arguments are specified
+    {
+        errno = 0;
+        char *p;
+        width = strtol(argv[2], &p, 10); // Convert second CLI argument to long and store it as width
+
+        if (errno != 0 || *p != '\0' || width > 100 || width < 1)
+        {
+            printf("Error processing your 2nd CLI argument (width of the tree) (int).\n");
+            printf("Enter value between 1-100.\n");
+            return 3;
+        }
+    }
+
+    if (argc >= 2) // If at least 'character' CLI argument is specified
+    {
+        character = argv[1][0]; // Store first character of CLI argument as character to create tree with
+    }
+
     // Execute until number of characters to write is greater or equal to provided width
+    // Each iteration increase numbers of characters to print by 2
     for (int i = width, chars_amount = 1; chars_amount <= width; chars_amount += 2)
     {
         for (int k = 0; k < ((i - chars_amount) / 2); k++) // Empty characters before
@@ -16,7 +37,7 @@ int main(int argc, char *argv[])
             printf(" ");
         }
 
-        for (int l = 0; l < chars_amount; l++) // Print out provided character 'chars_amount' times.
+        for (int l = 0; l < chars_amount; l++) // Print out provided character 'chars_amount' times
         {
             printf("%c", character);
         }
