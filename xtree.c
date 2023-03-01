ec90e8034cd85b6e1a24843438963e1192610b9d
R3Z0NE <rafox71321@gmail.com>
2023-03-01 01:11:42 +0100

    Improved output symmetry and code readability

--- xtree.c
+++ xtree.c
@@ -5,6 +5,9 @@
 
 int main(int argc, char *argv[])
 {
+    // If none CLI argument are privided, these variables remain untouched
+    long width = 21;      // Default width
+    char character = 'x'; // default character
 
     long width = 10; // Default width: 10
     char base = argv[1][0]; // TODO: Default character
@@ -23,7 +26,9 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    else
+    // Execute until number of characters to write is greater or equal to two-thirds of provided width
+    // Each iteration increase numbers of characters to print by 2
+    for (int chars_amount = 1; chars_amount <= width * 0.75; chars_amount += 2) // First part. Ends when width hits 75% of maximum value
     {
 
         // Execute until number of characters to write is greater or equal to provided width
@@ -39,11 +44,56 @@ int main(int argc, char *argv[])
                 printf("%c", base);
             }
 
-            for (int k = 0; k < ((i - chars_amount) / 2); k++) // Empty characters after
+        for (int k = 0; k < ((width - chars_amount) / 2); k++) // Empty characters after
+        {
+            printf(" ");
+        }
+        printf("\n"); // Next line
+    }
+
+    for (int chars_amount = 1; chars_amount <= width; chars_amount += 2) // Same loop but full width this time, and 'skip' some of the first iterations using 'if' statement
+    {
+        if (chars_amount > width * 0.2) // Start drawing second part of the tree from 20% of the width
+        {
+            for (int k = 0; k < ((width - chars_amount) / 2); k++)
             {
                 printf(" ");
             }
-            printf("\n"); // End of the line
+
+            for (int l = 0; l < chars_amount; l++)
+            {
+                printf("%c", character);
+            }
+
+            for (int k = 0; k < ((width - chars_amount) / 2); k++)
+            {
+                printf(" ");
+            }
+            printf("\n");
         }
     }
+
+    for (int trunk = width / 10, chars_amount = trunk; trunk >= 0; trunk--) // Trunk length. Always at least one.
+    {
+        if (chars_amount % 2 == 0) // If trunk width would be an even number, add one for keeping symmetry
+        {
+            chars_amount++;
+        }
+        for (int k = 0; k < ((width - chars_amount) / 2); k++) // Empty characters before
+        {
+            printf(" ");
+        }
+
+        for (int r = 0; r < chars_amount; r++) // Trunk width
+        {
+            printf("%c", character);
+        }
+
+        for (int k = 0; k < ((width - chars_amount) / 2); k++) // Empty characters after
+        {
+            printf(" ");
+        }
+        printf("\n"); // Next line
+    }
+    return 0;
 }
